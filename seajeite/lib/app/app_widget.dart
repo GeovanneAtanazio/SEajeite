80c282fca38de0b5cfa1e61a96cafb8b93ee96e1
Jonathan Kelvin <j_kelvin1997@hotmail.com>
2020-01-11 13:48:28 -0300

    utilizando constants ao inves de instanciar nv String

--- seajeite/lib/app/app_widget.dart
+++ seajeite/lib/app/app_widget.dart
@@ -1,11 +1,12 @@
 import 'package:flutter/material.dart';
 import 'package:seajeite/app/modules/home/home_module.dart';
+import 'package:seajeite/app/util/constants.dart';
 
 class AppWidget extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Seajeite',
+      title: APP_TITLE,
       theme: ThemeData(
         primarySwatch: Colors.blueGrey,
       ),
