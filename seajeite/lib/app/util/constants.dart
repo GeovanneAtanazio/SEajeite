98ed4a4e164debd34e52d9a36352a77ebc52872a
Jonathan Kelvin <j_kelvin1997@hotmail.com>
2020-01-11 13:45:13 -0300

    arquivo para aulixiar com String recorrentes

---
+++ seajeite/lib/app/util/constants.dart
@@ -0,0 +1,8 @@
+const APP_TITLE = "Seajeite";
+const NOTIFY_INTERVAL = 1;
+const NOTIFY_TIMES = 6;
+const NOTIFY_TEXT = "Arruma essa postura, seu arrombado!";
+const DESCRIPTION_NOTIFY_CHN = "Lembrete de correção postural";
+const DESCRIPTION_START = "Iniciar";
+const DESCRIPTION_STOP = "Parar";
+const DESCRIPTION_ACTIVITY_TIME = "TEMPO DE ATIVIDADE";
